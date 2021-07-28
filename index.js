59acb5e3120f455658a7b50d30588eaada9d01a9
SHRUTI RAJ <69633187+19shruti@users.noreply.github.com>
2021-07-28 14:57:48 +0530

    css: visibilityfinal

--- index.js
+++ index.js
@@ -1,56 +1,64 @@
-var btnEnter=document.querySelector("#btn-enter")
-var textInput=document.querySelector("#text-input");
-var cashReceived=document.querySelector("#cash-received");
-var outputBox=document.querySelector("#output");
-var outputCash=document.querySelector("#outputcash");
-var btncash=document.querySelector("#btn-cash");
-var subt=document.querySelector("#sub");
-var btnres=document.querySelector("#res");
- 
-function clickHandler(){
-   var inpuT=textInput.value;
-   outputBox.innerText=inpuT;
+
+
+let paid_Input = document.querySelector("#paid-Input");
+let cost_Input = document.querySelector("#cost-Input");
+let checkBtn = document.querySelector("#checkBtn");
+
+
+let twothousand_count = document.querySelector("#twothousand_count");
+let fivehund_count = document.querySelector("#fivehund_count");
+let hund_count = document.querySelector("#hund_count");
+let twenty_count = document.querySelector("#twenty_count");
+let ten_count = document.querySelector("#ten_count");
+let five_count = document.querySelector("#five_count");
+let one_count = document.querySelector("#one_count");
+
+    
+checkBtn.addEventListener("click", function () {
+
+    let paid = Number(paid_Input.value);
+    let cost = Number(cost_Input.value);
+    let total_change = (paid - cost);
+    alert("total change is ::  " + total_change);
+    let twothousand = Math.floor(total_change / 2000);
+    let left_over = (total_change % 2000);
+    let fivehund = Math.floor(left_over / 500);
+    left_over = (left_over % 500);
+    let hund = Math.floor(left_over / 100);
+    left_over = (left_over % 100);
+    let twenty = Math.floor(left_over / 20);
+    left_over = (left_over % 20);
+    let ten = Math.floor(left_over/ 10);
+    left_over = (left_over % 10);
+    let five = Math.floor(left_over/ 5);
+    left_over = (left_over % 5);
+    let one = left_over;
+
+    twothousand_count.innerHTML = twothousand;
+    fivehund_count.innerHTML = fivehund;
+    hund_count.innerHTML = hund;
+    twenty_count.innerHTML = twenty;
+    ten_count.innerHTML = ten;
+    five_count.innerHTML = five;
+    one_count.innerHTML = one;
+})
+// function setVisibility(checkBtn,output,text1,paid_Input,checkBtn)  {
+//     if(document.getElementById('cashBt1n').value=='Show Layer'){
+//         document.getElementById('cashBt1n').value = 'Hide Layer';
+//         document.getElementById('checkBtn').style.display = 'none';
+//         document.getElementById('output').style.display = 'none';
+//         }else{
+//         document.getElementById('cashBt1n').value = 'hide Layer';
+//         document.getElementById('checkBtn').style.display = 'inline';
+//         document.getElementById('output').style.display = 'inline';
+//         }
+//         }
+    
+function toggleVisibility(cashBt1n) {
+  
+    var e = document.getElementById(cashBt1n);
+    if(e.style.display == 'block' )
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
 }
-btnEnter.addEventListener("click",clickHandler);
-btncash.addEventListener("click",clickHandler1);
-btnres.addEventListener("click",sub);
-function clickHandler1(){
-    var cashReceive=cashReceived.value;
-    outputCash.innerText=cashReceive;
-}
-
-function sub(){
-    var n1=document.getElementById('text-input').value;
-    var n2=document.getElementById('cash-received').value;
-    var result=n2-n1;
-    subt.innerText=result;
-}
-const coins=[1,2,5,10,20,100,500,1000];
-let amount=sub();
-
-function change(amount, coins) {
-    if (amount === 0) {
-        return [0, []];
-    }
-    // if (coins.length === 0 && amount > 0) {
-    //     return [Infinity, []];
-    // }
-    if (coins[0] > amount) {
-        return change(amount, coins.slice(1));
-    } else {
-        var loseIt = change(amount, coins.slice(1));  // just one call of change
-        var useIt = change(amount - coins[0], coins); // just one call of change
-        if (loseIt[0] < 1 + useIt[0]) {
-            return loseIt;
-        } else {
-            return [1 + useIt[0], useIt[1].concat(coins[0])];
-        }
-    }
-} 
-
-// coins=[1,2,10,20,100,500,1000];
-// amount=sub();
-console.log(change());    
-
-
-
